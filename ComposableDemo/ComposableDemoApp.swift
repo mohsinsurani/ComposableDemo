f61a9faf59b3fe5678c28892b3a87c143f456cc0
Mohsin <mohsinmax786@gmail.com>
2024-02-16 05:00:10 +0000

    Initial Commit

---
+++ ComposableDemo/ComposableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableDemoApp.swift
+//  ComposableDemo
+//
+//  Created by Admin on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComposableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
